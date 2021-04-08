c31a5bcf0c47c119b52de61b86d4033223703672
Noah Emmons - neefly <nemmons@feature23.com>
2021-04-08 03:12:49 -0400

    Stopping point for now, starting to identify steps forward on, zoom and scrolling. Will take some more research on utilizing scrolling events and how to drag the canvas to change its offset.

--- src/App.js
+++ src/App.js
@@ -8,22 +8,7 @@ import Canvas from './components/Canvas'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <CandleStickCanvas />
-      {/* <Canvas /> */}
+      <CandleStickCanvas style={"marginTop: 10rem"}/>
     </div>
   );
 }
--- src/components/CandleStickCanvas.js
+++ src/components/CandleStickCanvas.js
@@ -1,34 +1,202 @@
 import React, {Component} from 'react';
+import $ from "jquery";
 
 class CandleStickCanvas extends Component {
 
-    constructor()
+    constructor(props)
     {
-        super();        
+        super(props);        
         
         this.state = {
             blah: 1,
             candleStickData: [],
-            canvasWidthScale: 1,
-            canvasHeightScale: 1
+            canvasWidthScale: 100,
+            canvasHeightScale: 100
         }
 
+        this.canvasRef = React.createRef();
         this.decrementScale = this.decrementScale.bind(this);
         this.incrementScale = this.incrementScale.bind(this);
         this.drawHorizontalGrid = this.drawHorizontalGrid.bind(this);
         this.drawRectangle = this.drawRectangle.bind(this);
         this.getCanvasContext = this.getCanvasContext.bind(this);
-        this.updateCanvasScale = this.updateCanvasScale.bind(this);
+        this.getCSVData = this.getCSVData.bind(this);
+        this.handleMouse = this.handleMouse.bind(this);
+    }
+
+    handleScroll(e)
+    {
+        debugger;
+    }
+
+    handleMouse(e)
+    {
+        console.log(e);
+    }
+
+    getCSVData() {
+        $.ajax({
+            type: "GET",
+            url: "My api that I can deploy and keep as the asset that could potentially farm and handle storing hard finance data",
+            dataType: "text",
+            success: function(data) {
+                debugger;
+                this.processData(data);
+            }
+        });
+    }
+    
+    processData(allText) {
+        var allTextLines = allText.split(/\r\n|\n/);
+        var headers = allTextLines[0].split(',');
+        var lines = [];
+    
+        for (var i=1; i<allTextLines.length; i++) {
+            var data = allTextLines[i].split(',');
+            if (data.length == headers.length) {
+    
+                var tarr = [];
+                for (var j=0; j<headers.length; j++) {
+                    tarr.push(headers[j]+":"+data[j]);
+                }
+                lines.push(tarr);
+            }
+        }
+
+        console.log(lines);
     }
 
     componentDidMount() {
+        //this.getCSVData();
 
         var ctx = this.getCanvasContext();
-        this.canvasExampleScene(ctx);
+        this.renderCandleStickExample(ctx);
     }
 
-    drawRectangle(ctx, x, y, width, height, isBullish)
-    {
+    componentDidUpdate() {
+        const ctx = this.getCanvasContext();
+        const cws = this.state.canvasWidthScale;
+        const chs = this.state.canvasHeightScale; 
+        ctx.scale(cws, chs);
+        this.renderCandleStickExample(ctx);
+    }
+
+    renderCandleStickExample(ctx) {
+
+        var cHeight = 540, cWidth = 960;
+
+        this.drawHorizontalGrid(ctx, 960, 540);
+
+        // for(var i = 0; i < 25; i++)
+        // {
+            //Calculate scale of the candlesticks by total number / viewsize
+            //Effective - $0
+            var effectiveBottom = Math.ceil(cHeight * 0.75); // if y == 405 == $0 = 1px = $1
+            ctx.lineTo(cWidth, Math.ceil(cHeight * 0.75));
+
+            //Given the following, how easily can we map this to the chart?
+            //How accurate? (bonus)
+            //Open	    High	Low	        Close       Adj Close
+            //74.370003	75.57	73.589996	75.540001	73.892517
+
+            var isBullish = this.getRandomInt(1, 25) % 2 ? true : false;
+
+            ctx.beginPath();
+            ctx.fillStyle = 'green';
+            ctx.strokeStyle = 'green';
+            ctx.lineStyle = 'green';
+            
+
+
+            var open = 74.37, close = 75.54, high = 75.57, low = 73.59, 
+            x = 1, y = 74.37, csWidth = 1, csHeight = close-open, scale = 10,
+            topShadow = close-high, bottomShadow=open-low;
+
+            ctx.fillRect(x, effectiveBottom - open, csWidth*scale, csHeight*scale);
+
+            var width = 2;
+
+            //COORDINATES WILL BE FLIPPED SINCE Y- = UP
+            //Top shadow
+            ctx.moveTo(x + ((csWidth*scale)/2), (effectiveBottom - y));
+            ctx.lineTo(x + ((csWidth*scale)/2), (effectiveBottom - y)-topShadow);
+
+            ctx.fillText("$" + close, 150, (effectiveBottom - y)); 
+            ctx.moveTo(x, (effectiveBottom - y)-topShadow);
+            ctx.lineTo(960, (effectiveBottom - y)-topShadow);
+
+            //Bottom Shadow
+            ctx.moveTo(x + ((csWidth*scale)/2), (effectiveBottom - y));
+            ctx.lineTo(x + ((csWidth*scale)/2), (effectiveBottom - y)+bottomShadow);
+
+            ctx.stroke();
+
+        // }
+    }
+
+    drawHorizontalGrid(ctx, cWidth, cHeight) {
+
+        var lines = Math.ceil(cHeight/25);
+
+        ctx.strokeStyle = 'black';
+
+        //Effective - $0
+        ctx.moveTo(0, Math.ceil(cHeight * 0.75));
+        ctx.lineTo(cWidth, Math.ceil(cHeight * 0.75));
+
+        var iterator = 0;
+        for(var i = Math.ceil(cHeight * 0.75); i >= 0; i--)
+        {
+            if(iterator % 50 === 0) {
+                ctx.lineFill = "grey";
+                ctx.fillText("$" + iterator, cWidth - 35, i+10); 
+                ctx.moveTo(0, i);
+                ctx.lineTo(cWidth, i);
+                ctx.stroke();
+            }
+
+            iterator++;
+        }
+
+        ctx.stroke();
+    }
+
+    getCanvasContext() {
+        const canvas = this.canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        const width = canvas.width;
+        const height = canvas.height;
+        ctx.scale(this.state.canvasWidthScale, this.state.canvasHeightScale);
+        ctx.save();
+        ctx.beginPath();
+        ctx.clearRect(0, 0, width, height);
+
+        if(ctx !== null || ctx !== undefined)
+            return ctx;
+
+        return null;
+    }
+
+    canvasExampleScene(ctx) {
+        this.drawHorizontalGrid(ctx, 960, 540);
+        this.drawRectangle(ctx, 0, 0, 100, 100, true)
+    }
+
+    decrementScale() {
+        this.setState(prevState => ({
+            canvasWidthScale: prevState.canvasWidthScale /= 1.1,
+            canvasHeightScale: prevState.canvasHeightScale /= 1.1,
+        }));
+    }
+
+    incrementScale() {
+        this.setState(prevState => ({
+            canvasWidthScale: prevState.canvasWidthScale *= 1.1,
+            canvasHeightScale: prevState.canvasHeightScale *= 1.1,
+        }));
+    }
+
+    drawRectangle(ctx, x, y, width, height, isBullish) {
         ctx.beginPath();
         ctx.fillStyle = isBullish ? 'green' : 'red';
         ctx.strokeStyle = isBullish ? 'green' : 'red';
@@ -48,30 +216,7 @@ class CandleStickCanvas extends Component {
         ctx.stroke();
     }
 
-    getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    drawHorizontalGrid(ctx, width, height)
-    {
-
-        var lines = Math.ceil(height/25);
-
-        ctx.strokeStyle = 'black';
-
-        for(var i = 0; i < lines; i++)
-        {
-            ctx.moveTo(0, i*25);
-            ctx.lineTo(width, i*25);
-        }
-
-        ctx.stroke();
-    }
-
-    drawVerticalGrid(ctx, width, height)
-    {
+    drawVerticalGrid(ctx, width, height) {
 
         var lines = Math.ceil(width/50);
 
@@ -84,8 +229,7 @@ class CandleStickCanvas extends Component {
         ctx.stroke();
     }
 
-    drawStokeSquareOnCanvas(ctx, x, y, sideLength)
-    {
+    drawStokeSquareOnCanvas(ctx, x, y, sideLength) {
 
         var topRightPoint = x + sideLength;
         var bottomYPoint = y + sideLength
@@ -112,56 +256,18 @@ class CandleStickCanvas extends Component {
         ctx.stroke();
     }
 
-    decrementScale(e)
-    {
-        this.setState(state => ({
-            canvasWidthScale: state.canvasWidthScale -= .1,
-            canvasHeightScale: state.canvasHeightScale -= .1,
-        }));
-
-        this.updateCanvasScale();
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    incrementScale(e)
-    {
-        this.setState(state => ({
-            canvasWidthScale: state.canvasWidthScale += .1,
-            canvasHeightScale: state.canvasHeightScale += .1,
-        }), this.updateCanvasScale());
-    }
-
-    updateCanvasScale(){
-        var ctx = this.getCanvasContext();
-        ctx.scale(this.state.canvasWidthScale, this.state.canvasHeightScale);
-        this.canvasExampleScene(ctx);
-    };
-
-    getCanvasContext()
-    {
-        var c = document.getElementById("myCanvas") !== null ? document.getElementById("myCanvas") : null;
-        var ctx = c !== null ? c.getContext("2d") : null;
-        ctx.clearRect(0, 0, c.width, c.height);
-
-        if(ctx !== null || ctx !== undefined)
-            return ctx;
-
-        return null;
-    }
-
-    canvasExampleScene(ctx)
-    {
-        this.drawHorizontalGrid(ctx, 960, 540);
-        this.drawRectangle(ctx, 0, 0, 100, 100, true)
-    }
-
-    render()
-    {        
+    render() {        
         var disableDecrease = this.state.canvasWidthScale <= 1 ? true:false;
-        //disabled={disableDecrease}
         return(
             <div>
-                <canvas id="myCanvas" width={960} height={540} style={{border: "1px solid #000000"}}></canvas>
-                <input type="button" onClick={this.decrementScale} value="-" />
+                <canvas id="myCanvas" ref={this.canvasRef} width={960} height={540} onClick={this.handleMouse} onscroll={this.handleScroll} style={{border: "1px solid #000000"}}></canvas>
+                <input type="button" onClick={this.decrementScale} value="-"/>
                 <input type="button" onClick={this.incrementScale} value="+"/>
             </div>
         );
